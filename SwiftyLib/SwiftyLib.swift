edf57e97d3e5f3f95d68a935cced3437630f441d
ParkhomenkoAlexey <parkhomenko_ad@mail.ru>
2020-12-16 17:49:13 +0500

    Added SwiftyLib.podspec

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Пархоменко Алексей on 16.12.2020.
+//
+
+import Foundation
+
+public class SwiftyLib {
+    
+    public init(){}
+    
+    public func hello() {
+        print(#function)
+    }
+}
